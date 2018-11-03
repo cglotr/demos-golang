3d7b29ae1c02233bf09a28bb916bdee5739b0c00
cglotr <amir.ariffin.920404@gmail.com>
2018-11-03 10:20:47 +0800

    post & delete employee

--- crud-mysql/main.go
+++ crud-mysql/main.go
@@ -2,10 +2,59 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type employee struct {
+	City string `json:"city"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	defer db.Close()
+
+	var employee employee
+	log.Println(r.Body)
+	_ = json.NewDecoder(r.Body).Decode(&employee)
+	st, err := db.Prepare("INSERT INTO employee (name, city) VALUES (?,?)")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	st.Exec(employee.Name, employee.City)
+	log.Printf("INSERT employee name: %s, city: %s\n", employee.Name, employee.City)
+}
+
+func deleteEmployee(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	defer db.Close()
+
+	params := mux.Vars(r)
+	st, err := db.Prepare("DELETE FROM employee WHERE id = ?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	employeeID, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	st.Exec(employeeID)
+	log.Printf("DELETE employee id: %d\n", employeeID)
+}
 
 func dbConn() (db *sql.DB) {
 	dbDriver := "mysql"
@@ -23,6 +72,10 @@ func dbConn() (db *sql.DB) {
 }
 
 func main() {
-	db := dbConn()
-	defer db.Close()
+	router := mux.NewRouter()
+
+	router.HandleFunc("/employees", createEmployee).Methods("POST")
+	router.HandleFunc("/employees/{id}", deleteEmployee).Methods("DELETE")
+
+	http.ListenAndServe(":8000", router)
 }
