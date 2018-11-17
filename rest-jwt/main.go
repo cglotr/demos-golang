4878d6494340eab8a9ff5a55d3e1eab0f432261c
cglotr <amir.ariffin.920404@gmail.com>
2018-11-17 13:17:31 +0800

    get dummy products

--- rest-jwt/main.go
+++ rest-jwt/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
+
+type product struct {
+	Description string
+	ID          int
+	Name        string
+	Slug        string
+}
+
+var products = []product{
+	product{
+		Description: "Test product.",
+		ID:          1,
+		Name:        "Test 001",
+		Slug:        "test-001",
+	},
+	product{
+		Description: "Test product.",
+		ID:          2,
+		Name:        "Test 002",
+		Slug:        "test-002",
+	},
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -15,7 +38,7 @@ func main() {
 	r.PathPrefix("/static/").Handler(
 		http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
-	r.Handle("/products", notImplemented).Methods("GET")
+	r.Handle("/products", productsHandler).Methods("GET")
 	r.Handle("/products/{slug}/feedback", notImplemented).Methods("POST")
 	r.Handle("/status", notImplemented).Methods("GET")
 
@@ -24,4 +47,10 @@ func main() {
 
 var notImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not Implemented"))
+})
+
+var productsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	payload, _ := json.Marshal(products)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
 })
