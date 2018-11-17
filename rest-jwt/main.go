04fa4bc42d1a5788a788422ee021200453416c38
cglotr <amir.ariffin.920404@gmail.com>
2018-11-17 13:02:32 +0800

    empty api

--- rest-jwt/main.go
+++ rest-jwt/main.go
@@ -15,5 +15,13 @@ func main() {
 	r.PathPrefix("/static/").Handler(
 		http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
+	r.Handle("/products", notImplemented).Methods("GET")
+	r.Handle("/products/{slug}/feedback", notImplemented).Methods("POST")
+	r.Handle("/status", notImplemented).Methods("GET")
+
 	http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
 }
+
+var notImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
+})
