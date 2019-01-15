8a81685f9e624c7c66c3447152d3cb52ea6ccde6
cglotr <amir.ariffin.920404@gmail.com>
2019-01-15 22:35:10 +0800

    user name

--- better-websocket-chat/main.go
+++ better-websocket-chat/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/cglotr/websocket"
 	"github.com/gorilla/mux"
 )
 
-var broadcastChan = make(chan string)
-var clients = make(map[*websocket.Conn]bool)
+var broadcastChan = make(chan string, 2)
+var clients = make(map[*websocket.Conn]string)
 var upgrader = websocket.Upgrader{}
 
 func main() {
@@ -23,10 +25,15 @@ func main() {
 
 func broadcast() {
 	for {
+		name := <-broadcastChan
 		msg := <-broadcastChan
 
 		for conn := range clients {
-			err := conn.WriteMessage(1, []byte(msg))
+			if clients[conn] == name {
+				continue
+			}
+
+			err := conn.WriteMessage(1, []byte(name+": "+msg))
 			if err != nil {
 				conn.Close()
 				delete(clients, conn)
@@ -42,7 +49,7 @@ func chat(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer conn.Close()
-	clients[conn] = true
+	clients[conn] = ""
 
 	for {
 		_, msg, err := conn.ReadMessage()
@@ -52,6 +59,21 @@ func chat(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
-		broadcastChan <- string(msg[:len(msg)])
+		msgStr := string(msg[:len(msg)])
+
+		if m, _ := regexp.MatchString(":name\\s.*", msgStr); m {
+			name := strings.Split(msgStr, " ")[1]
+			clients[conn] = name
+			continue
+		}
+
+		if clients[conn] == "" {
+			conn.WriteMessage(1, []byte("Tell me your name first."))
+			conn.WriteMessage(1, []byte("Use the command :name <your_name>."))
+			continue
+		}
+
+		broadcastChan <- clients[conn]
+		broadcastChan <- msgStr
 	}
 }
